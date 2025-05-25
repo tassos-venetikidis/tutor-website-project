daf268b947c93f16b5c824f47fa95e46e7d51e6c
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-05-25 15:04:37 +0300

    Added the navbar background change on scroll

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// Toggle mobile menu
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerButton = document.querySelector(".navbar .mobile-menu-toggle");
   const mobileMenuItems = document.querySelector(".navbar .mobile-menu-items");
@@ -5,4 +6,14 @@ document.addEventListener("DOMContentLoaded", function () {
   hamburgerButton.addEventListener("click", () => {
     mobileMenuItems.classList.toggle("active");
   });
+});
+
+// Change navbar background on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (this.window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
 });
